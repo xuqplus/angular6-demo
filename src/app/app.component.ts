f48a74f07403413a1ebad9e4ed79d8609b3f7d8b
qunqun.xu <qunqun.xu@counect.com>
2018-07-05 16:25:53 +0800

    add jquery, add bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
+declare var $: any;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    title = 'app';
+
+    aa(): void {
+        $('#id').html('yes');
+    }
 }
